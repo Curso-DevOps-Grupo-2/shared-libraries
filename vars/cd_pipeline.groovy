da31f1dd25636c496a075eb726e6ae1ba3b84988
Diego Roco <droco@amisoft.cl>
2022-01-29 20:31:44 -0300

    prueba

--- vars/cd_pipeline.groovy
+++ vars/cd_pipeline.groovy
@@ -44,7 +44,8 @@ def nexusDownload(){
             script: "mvn help:evaluate -Dexpression=project.version | grep -e '^[^[]'", returnStdout: true
         )
         def URL = "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/'''${version}'''/DevOpsUsach2020-'''${version}'''.jar"
-        sh "echo '${URL.trim()}'"
+        def string = URL.replaceAll("\\n\\r", "")
+        sh "echo '${string}'"
         sh "echo '${version}'"
         sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD $URL -O"
     }
