1614c708c8b02b1321c3d41de3d89daeec92f548
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-01-29 18:36:04 -0300

    fix regex command

--- vars/cd_pipeline.groovy
+++ vars/cd_pipeline.groovy
@@ -40,7 +40,7 @@ def nexusDownload(){
     env.STAGE = "Stage 2: nexus download"
     stage("$env.STAGE"){
         sh "echo 'download from nexus'"
-        sh "export PVERSION=`mvn help:evaluate -Dexpression=project.version | grep -e '^[^\[]'`"
+        sh "export PVERSION=`mvn help:evaluate -Dexpression=project.version | grep -e '^[^[]'`"
         sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASS 'http://localhost:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/'{$PVERSION}'/DevOpsUsach2020-'{$PVERSION}'.jar' -O"
     }
 }
