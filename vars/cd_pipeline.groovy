b57f7aca10341406538ac4c799f804d9d773d890
Diego Roco <droco@amisoft.cl>
2022-01-29 19:31:30 -0300

    5

--- vars/cd_pipeline.groovy
+++ vars/cd_pipeline.groovy
@@ -41,6 +41,7 @@ def nexusDownload(){
     stage("$env.STAGE"){
         sh "echo 'download from nexus'"
         sh "export PVERSION=`mvn help:evaluate -Dexpression=project.version | grep -e '^[^[]'`"
+        sh "echo $PVERSION"
         sh "curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD 'http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/' + $PVERSION + '/DevOpsUsach2020-' + $PVERSION + '.jar' -O"
     }
 }
