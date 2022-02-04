6beeb4bb410f2fe58d7a6acd71eb4bda365f31fc
Diego Rodriguez Mancini <diegorodriguezmancini@gmail.com>
2022-02-03 21:12:23 -0300

    add slack message

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -15,22 +15,31 @@ def call(){
                 steps {
                     script{                        
                         sh "env"
-
                         def versionUtils = new version.versionUtils();
                         def nextVersion = versionUtils.getNextVersion('minor')
                         def currentVersion = versionUtils.getCurrentVersion()
                         if (env.GIT_BRANCH.contains("feature") || env.GIT_BRANCH.contains("develop")) {
                             figlet  "C. INTEGRATION"
+                            env.PIPELINE_TYPE = "CI"
                             ci_pipeline.call(params.stages, nextVersion, currentVersion)
                         }
                         else if (env.GIT_BRANCH.contains("release")) {
                             figlet  "C. DELIVERY"
+                            env.PIPELINE_TYPE = "CD"
                             cd_pipeline.call(params.stages, nextVersion, currentVersion)
                         }
                         else{
                             sh "echo 'Rama no identificada para ejecutar un pipeline.'"
                         }
                         
+                    }
+                }
+                post{
+                    success{
+                        slackSend color: 'good', message: "[Grupo 2] [Pipeline ${env.PIPELINE_TYPE}] [${env.GIT_BRANCH}] Ejecucion Exitosa 🎉", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
+                    }
+                    failure{
+                        slackSend color: 'danger', message: "[Grupo 2] [Pipeline ${env.PIPELINE_TYPE}] [${env.GIT_BRANCH}] Ejecucion Fallida 😢", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-token'
                     }
                 }
             }
